e44dbb2e87e23b09261c6f867fc7efd6d858514d
mahmoud helmy <m.7elmy95@outlook.com>
2020-12-03 22:30:14 +0200

    updates

---
+++ Initial_Clean_Architecture.Helpers/Constants/ApiUrlsConst/ApiBaseUrlConst.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Initial_Clean_Architecture.Helpers.Constants.ApiUrlsConst
+{
+    public class ApiBaseUrlConst
+    {
+        private const string _version = "v1";
+        internal readonly string _baseUrl = "api/" + _version + "/";
+    }
+}
---
+++ Initial_Clean_Architecture.Helpers/Installers/Extensions/InstallerServiceExtension.cs
@@ -0,0 +1,23 @@
+﻿using Initial_Clean_Architecture.Helpers.Installers.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Initial_Clean_Architecture.Helpers.Installers.Extensions
+{
+    public static class InstallerServiceExtension
+    {
+        public static void InstallServices(this IServiceCollection services, IConfiguration configuration, Assembly assembly)
+        {
+            var installers = assembly.ExportedTypes.Where(x =>
+              typeof(IServicesInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract).Select(
+                Activator.CreateInstance).Cast<IServicesInstaller>().ToList();
+
+            installers.ForEach(installer => installer.InstallServices(services, configuration));
+        }
+    }
+}
